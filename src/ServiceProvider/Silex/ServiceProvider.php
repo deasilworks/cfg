58fefded90bdcd40d11ba7bb4948a21177d53b52
Craig Johnston <cjimti@gmail.com>
2017-07-29 16:28:32 -0700

    bootstrapped cms, abstracted silex/pimple configuration for cms

---
+++ src/ServiceProvider/Silex/ServiceProvider.php
@@ -0,0 +1,79 @@
+<?php
+
+/*
+ * MIT License
+ *
+ * Copyright (c) 2017 Deasil Works, Inc
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+namespace deasilworks\cfg\ServiceProvider\Silex;
+
+use Pimple\Container;
+use Symfony\Component\Serializer\NameConverter\CamelCaseToSnakeCaseNameConverter;
+
+/**
+ * Class CEFServiceProvider.
+ *
+ * Responsible for providing config population
+ */
+abstract class ServiceProvider
+{
+    /**
+     * @var string
+     */
+    protected $namespace;
+
+    /**
+     * CEFServiceProvider constructor.
+     * @param string $namespace
+     */
+    public function __construct($namespace = 'deasilworks')
+    {
+        $this->namespace = $namespace;
+    }
+
+    /**
+     * @param Container $container
+     * @param string $serviceKey
+     */
+    protected function populateConfig($serviceKey, Container $container)
+    {
+        $config = $container[$this->namespace . '.'. $serviceKey .'.config'];
+        $configMethods = get_class_methods(get_class($config));
+        $converter = new CamelCaseToSnakeCaseNameConverter();
+
+        // Check container keys matching config setters.
+        //
+        foreach ($configMethods as $method) {
+            $matches = [];
+            preg_match('/^set_(.+)/', $converter->normalize($method), $matches);
+
+            if (count($matches) < 1) {
+                continue;
+            }
+
+            $configKey = $this->namespace . '.' . $serviceKey .'.' . $matches[1];
+
+            if (isset($container[$configKey])) {
+                $config->$method($container[$configKey]);
+            }
+        }
+    }
+}
